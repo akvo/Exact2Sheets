66b3ccccb2e5b7cb55c886e4ec53f6a12859cb63
valeria <valerie.cardoso@gmail.com>
2020-11-10 10:38:57 +0100

    [#3] extracted db config

--- src/org/akvo/exact/repository/DataBaseFactory.kt
+++ src/org/akvo/exact/repository/DataBaseFactory.kt
@@ -1,14 +1,24 @@
 package org.akvo.exact.repository
 
+import com.typesafe.config.ConfigFactory
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.config.*
+import io.ktor.util.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
+@KtorExperimentalAPI
 object DataBaseFactory {
+
+    private val secrets = HoconApplicationConfig(ConfigFactory.load("secret.conf"))
+    private val databaseName = secrets.property("ktor.database.name").getString()
+    private val databaseUser = secrets.property("ktor.database.user").getString()
+    private val databaseUsername = secrets.property("ktor.database.user").getString()
+    private val databaseUserPassword = secrets.property("ktor.database.password").getString()
 
     fun init() {
         Database.connect(hikari())
@@ -21,19 +31,15 @@ object DataBaseFactory {
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = "org.postgresql.Driver"
-        config.jdbcUrl = "jdbc:postgresql:users?user=postgres"
+        config.jdbcUrl = "jdbc:postgresql:$databaseName?user=$databaseUser"
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-        val user = System.getenv("DB_USER")
-        if (user != null) {
-            config.username = user
+        if (databaseUsername != null && databaseUsername.isNotEmpty()) {
+            config.username = databaseUsername
         }
-        val password = System.getenv("DB_PASSWORD")
-        if (password != null) {
-            config.password = password
-        } else {
-            config.password = "postgres"
+        if (databaseUserPassword != null) {
+            config.password = databaseUserPassword
         }
         config.validate()
         return HikariDataSource(config)
