5a8995c54c5d528f64134aa5abed562a9fbdf662
valeria <valerie.cardoso@gmail.com>
2020-11-10 12:46:37 +0100

    [#3] getting full url from secret

--- src/org/akvo/exact/repository/DataBaseFactory.kt
+++ src/org/akvo/exact/repository/DataBaseFactory.kt
@@ -15,7 +15,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 object DataBaseFactory {
 
     private val secrets = HoconApplicationConfig(ConfigFactory.load("secret.conf"))
-    private val password = secrets.property("ktor.database.password").getString()
+    private val url = secrets.property("ktor.database.url").getString()
 
     fun init() {
         Database.connect(hikari())
@@ -27,7 +27,7 @@ object DataBaseFactory {
 
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
-        config.jdbcUrl = "jdbc:postgresql://none/exact2sheets?user=exact2sheetsuser&password=$password&ssl=false&cloudSqlInstance=akvo-lumen:europe-west1:shared-test-database&socketFactory=com.google.cloud.sql.postgres.SocketFactory"
+        config.jdbcUrl = url
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
